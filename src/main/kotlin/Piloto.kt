ddfb1b8857575c619bf3c450b5e83b3f7623764a
Juancete <juan.contardo@gmail.com>
2019-11-06 16:52:18 -0300

    Initial commit

---
+++ src/main/kotlin/Piloto.kt
@@ -0,0 +1,26 @@
+const val MULTIPLICADOR_PAR = 10
+const val MULTIPLICADOR_IMPAR = 9
+
+const val VALOR_BASE = 30
+
+class Piloto(var formaDeConducir: (Pista) -> Int) {
+    fun tiempoDeVuelta(pista: Pista): Int = this.formaDeConducir(pista)
+}
+
+val audaz =  {tiempoCurva: Int ->{ pista: Pista -> pista.cantidadCurvas * tiempoCurva * pista.largoPorVuelta}}
+
+val virtuoso =  {nivelVirtuosismo: Int -> { pista: Pista ->  pista.largoPorVuelta * (VALOR_BASE / nivelVirtuosismo)}}
+
+val cabulero = {pista: Pista -> pista.largoPorVuelta * efectoMultiplicador(pista)}
+
+fun efectoMultiplicador (pista: Pista): Int {
+    return if (pista.nombrePar())
+        MULTIPLICADOR_PAR
+    else
+        MULTIPLICADOR_IMPAR
+}
+
+class Pista(private val nombre: String, val largoPorVuelta: Int, val cantidadCurvas: Int) {
+
+    fun nombrePar(): Boolean = this.nombre.length % 2 == 0
+}
---
+++ src/test/kotlin/tests.kt
@@ -0,0 +1,29 @@
+package org.uqbar.rapidosobjetosos.domain
+
+import Piloto
+import Pista
+import audaz
+import cabulero
+import kotlin.test.*
+import org.junit.Test as test
+
+
+class TestPilotoCabulero() {
+    val pilotoCabulero = Piloto(cabulero)
+    val pilotoAudaz = Piloto(audaz(3))
+
+    @test fun `en una pista par multiplica por 10` () {
+    val pistaPar =  Pista("Monaco", 5, 5)
+    assertEquals(50,pilotoCabulero.tiempoDeVuelta(pistaPar))
+    }
+
+    @test fun `en una pista impar multiplica por 9`() {
+        val pistaImpar = Pista("Estoril", 5,5)
+        assertEquals(45,pilotoCabulero.tiempoDeVuelta(pistaImpar))
+    }
+
+    @test fun `en una pista común`() {
+        val pista = Pista("Estoril", 5, 5)
+        assertEquals(75, pilotoAudaz.tiempoDeVuelta(pista))
+    }
+}
