94a9cba23495405f6a8e4603c8a47441cf7c5174
Juancete <juan.contardo@gmail.com>
2019-11-07 09:09:28 -0300

    format code

--- src/main/kotlin/Piloto.kt
+++ src/main/kotlin/Piloto.kt
@@ -7,13 +7,13 @@ class Piloto(var formaDeConducir: (Pista) -> Int) {
     fun tiempoDeVuelta(pista: Pista): Int = this.formaDeConducir(pista)
 }
 
-val audaz =  {tiempoCurva: Int ->{ pista: Pista -> pista.cantidadCurvas * tiempoCurva * pista.largoPorVuelta}}
+val audaz = { tiempoCurva: Int -> { pista: Pista -> pista.cantidadCurvas * tiempoCurva * pista.largoPorVuelta } }
 
-val virtuoso =  {nivelVirtuosismo: Int -> { pista: Pista ->  pista.largoPorVuelta * (VALOR_BASE / nivelVirtuosismo)}}
+val virtuoso = { nivelVirtuosismo: Int -> { pista: Pista -> pista.largoPorVuelta * (VALOR_BASE / nivelVirtuosismo) } }
 
-val cabulero = {pista: Pista -> pista.largoPorVuelta * efectoMultiplicador(pista)}
+val cabulero = { pista: Pista -> pista.largoPorVuelta * efectoMultiplicador(pista) }
 
-fun efectoMultiplicador (pista: Pista): Int = if (pista.nombrePar())  MULTIPLICADOR_PAR else MULTIPLICADOR_IMPAR
+fun efectoMultiplicador(pista: Pista): Int = if (pista.nombrePar()) MULTIPLICADOR_PAR else MULTIPLICADOR_IMPAR
 
 class Pista(private val nombre: String, val largoPorVuelta: Int, val cantidadCurvas: Int) {
 
